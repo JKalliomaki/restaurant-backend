4217bcc8803b5d04ca09c4861096a754cf8d0004
Jussi Kalliomäki <jussi.kalliomaki99@gmail.com>
2020-06-09 00:40:17 +0300

    gitignore update

---
+++ build/precache-manifest.43567cd1f32016c9050e22b3f07f081c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcd903787939fd9c8fc35888d98d50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e31fd11b35b3b5656ba",
+    "url": "/static/js/2.e32b3d3f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.e32b3d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060f29b322bb84c4e58b",
+    "url": "/static/js/main.93d43a0b.chunk.js"
+  },
+  {
+    "revision": "1ca97a53488c596a0fff",
+    "url": "/static/js/runtime-main.79c487b5.js"
+  }
+]);
